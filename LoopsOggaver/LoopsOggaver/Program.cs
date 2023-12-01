dc2dc8fc7af7d6a9e79fad1318b029bd21d83ef1
simenbs <141590049+simenbs@users.noreply.github.com>
2023-12-01 13:51:13 +0100

    Add files via upload

---
+++ LoopsOggaver/LoopsOggaver/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LoopsOggaver
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Terje er kul");
+            }
+
+        }
+    }
+}
