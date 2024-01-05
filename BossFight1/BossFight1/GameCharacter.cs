7c1f7099c85584769dc409db7ed524cd20ea9a13
Simen Baarsrud Sjåmo <141590049+simenbs@users.noreply.github.com>
2024-01-05 12:14:33 +0100

    Add files via upload

---
+++ BossFight1/BossFight1/GameCharacter.cs
@@ -0,0 +1,60 @@
+﻿namespace BossFight1
+{
+    public class GameCharacter
+    {
+        // Gamecharacter klassen har 4 private variabler
+        // initialstamina er nyttig for å når karakterene trenger å lade opp utholdenheten
+        private int _health;
+
+        private int _strength;
+
+        private int _stamina;
+
+        private int _initialStamina; // lage en initial helse verdi for å kunne bruke i recharge metode
+
+
+        // Konstruktør for å initialisere verdiene når det blir opprettet.
+        public GameCharacter(int health, int strength, int stamina)
+        {
+            _health = health;
+            _strength = strength;
+            _stamina = stamina;
+            _initialStamina = stamina;
+        }
+
+        //Her gjør jeg de private variablene tilgjengelig
+        public int Health { get { return _health; } }
+        public int Strength { get { return _strength; } }
+
+        public int Stamina { get { return _stamina; } }
+
+        public void Fight(GameCharacter opponent)
+        {
+            //sjekker om karakteren har nok helse til å sloss
+            if (_stamina <= 0)
+            {
+                // dersom den ikke har det vil den regenerere
+                Recharge();
+                return;
+            }
+
+            int damage = _strength; //for å legge til at skaden er lik strength
+            opponent._health -= damage; //fjerne helse fra motstanderen per slag
+            _stamina -= 10; //hver gang en karakter bruker fight metode mister den 10 helse
+
+        }
+
+        //metoden for å regenerere
+        public void Recharge()
+        {
+            _stamina = _initialStamina;
+        }
+
+        public void RandomStrenght()
+        {
+            Random rnd = new Random();
+            _strength = rnd.Next(0,31);
+        }
+        
+    }
+}
---
+++ BossFight1/BossFight1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace BossFight1
+{
+    internal class Program
+    {
+        //dette programmet simulerer en kamp mellom en helt og en boss, men siden bossen har mye mer verdier
+        //vil det være veldig liten sjangs for at bossen kan vinne jomindre helse justeres
+        static void Main(string[] args)
+        {
+            //lager en helt og en boss fra gamecharacter klassen
+            GameCharacter hero = new GameCharacter(100, 20, 40);
+            GameCharacter boss = new GameCharacter(400, 20, 10);
+
+            //selve kampen som er satt til å fortsette helt til noen ikke har mer helse
+            while (hero.Health > 0 && boss.Health > 0)
+            {
+                //hvem som angriper, samtidig som det blir logget til konsollen
+                hero.Fight(boss);
+                Console.WriteLine($"Helten slo bossen for {hero.Strength} skade, bossen har nå {boss.Health} helse igjen");
+
+                if (boss.Health <= 0)
+                    break;
+
+                boss.RandomStrenght();
+                boss.Fight(hero);
+                Console.WriteLine($"Bossen slo helten for {boss.Strength} skade, helten har nå {hero.Health} helse igjen");
+            }
+
+            //hvem som vant kampen
+            if (hero.Health > 0)
+
+                Console.WriteLine("Helten vinner kampen");
+            else
+                Console.WriteLine("Bossen vinner kampen");
+
+        }
+    }
+
+
+}
