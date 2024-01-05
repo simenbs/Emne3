e1da785eb58b5c6d6ab798c4d7ca188a1ae2c800
Simen Baarsrud Sjåmo <141590049+simenbs@users.noreply.github.com>
2024-01-05 11:55:06 +0100

    Add files via upload
    Parprogrammering med Tommy

---
+++ PPTommyUke7/PPTommyUke7/InventoryItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPTommyUke7
+{
+     public abstract class InventoryItem
+
+    {
+        public string Navn { get; set; }
+
+        public int Antall { get; set; }
+
+        public double Pris { get; set; }
+
+        protected InventoryItem(string navn, int antall, double pris)
+        {
+            Navn = navn;
+            Antall = antall;
+            Pris = pris;
+        }
+
+        public virtual void PrintItems()
+        {
+            Console.WriteLine($"Vare: {Navn}\n" +
+                              $"Antall: {Antall}\n" +
+                              $"Pris: {Pris:C}");
+        }
+    }
+     
+}
